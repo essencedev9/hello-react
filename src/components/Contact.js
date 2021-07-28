bb9fe8f057156077b674517bf7ac57fe3722d502
essencedev9 <essencedev9@nntuple.com>
2021-07-28 17:56:49 +0900

    Add Function - Contact 검색, 선택

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ContactInfo from "./ContactInfo";
+import ContactDetails from "./ContactDetails";
 
 export default class Contact extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      selectedKey: -1,
+      keyword: "",
       contactData: [
         { name: "Abet", phone: "010-0000-0001" },
         { name: "Betty", phone: "010-0000-0002" },
@@ -12,20 +15,57 @@ export default class Contact extends React.Component {
         { name: "David", phone: "010-0000-0004" }
       ]
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      keyword: e.target.value
+    });
+  }
+
+  handleClick(key) {
+    this.setState({
+      selectedKey: key
+    });
+
+    console.log(key, "is selected");
   }
 
   render() {
     const mapToComponent = data => {
+      data.sort();
+
+      data = data.filter(contact => {
+        return contact.name.toLowerCase().indexOf(this.state.keyword) > -1;
+      });
+
       return data.map((contact, i) => {
-        return <ContactInfo contact={contact} key={i} />;
+        return (
+          <ContactInfo
+            contact={contact}
+            key={i}
+            onClick={() => this.handleClick(i)}
+          />
+        );
       });
     };
 
     return (
       <div>
         <h1>Contacts</h1>
-        <input name="keyword" placeholder="Search" />
+        <input
+          name="keyword"
+          placeholder="Search"
+          value={this.state.keyword}
+          onChange={this.handleChange}
+        />
         <div>{mapToComponent(this.state.contactData)}</div>
+        <ContactDetails
+          isSelected={this.state.selectedKey !== -1}
+          contact={this.state.contactData[this.state.selectedKey]}
+        />
       </div>
     );
   }
