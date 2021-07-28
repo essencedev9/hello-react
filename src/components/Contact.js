6580b5edd59050cb9d96cebb40bad5fc0a73c4ae
essencedev9 <essencedev9@nntuple.com>
2021-07-28 18:34:24 +0900

    Add Function - Create, Update, Remove Array

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,8 @@ import React from "react";
 import ContactInfo from "./ContactInfo";
 import ContactDetails from "./ContactDetails";
 
+import update from "react-addons-update";
+
 export default class Contact extends React.Component {
   constructor(props) {
     super(props);
@@ -17,6 +19,10 @@ export default class Contact extends React.Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+
+    this.handleCreate = this.handleCreate.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
   }
 
   handleChange(e) {
@@ -31,6 +37,32 @@ export default class Contact extends React.Component {
     });
 
     console.log(key, "is selected");
+  }
+
+  handleCreate(contact) {
+    this.setState({
+      contactData: update(this.state.contactData, { $push: [contact] })
+    });
+  }
+
+  handleRemove() {
+    this.setState({
+      contactData: update(this.state.contactData, {
+        $splice: [this.state.selectedKey, 1]
+      }),
+      selectedKey: -1
+    });
+  }
+
+  handleUpdate(name, phone) {
+    this.setState({
+      contactData: update(this.state.contactData, {
+        [this.state.selectedKey]: {
+          name: { $set: name },
+          phone: { $set: phone }
+        }
+      })
+    });
   }
 
   render() {
